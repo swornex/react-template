5c377a785b2339bed0455944e746cdb42cf7e826
Swornima Balay Shrestha <sworstha@gmail.com>
2024-02-04 21:45:16 +0545

    remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-// import Timer from './components/timer/Timer';
 import Weather from './components/weather/Weather';
 
 export const App = () => {
   return (
     <>
-      {/* <Timer /> */}
       <Weather />
     </>
   );
